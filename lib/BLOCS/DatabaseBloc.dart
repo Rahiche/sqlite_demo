536b91bc207ef6dbe4de5876f205bcacd425fef5
rahiche <rahihce.omar.rahiche@gmail.com>
2020-07-12 12:17:39 +0100

    fix some outdated code

--- lib/BLOCS/DatabaseBloc.dart
+++ lib/BLOCS/DatabaseBloc.dart
@@ -4,34 +4,34 @@ import 'package:sqlite_demo/ClientModel.dart';
 import 'package:sqlite_demo/Database.dart';
 
 class ClientsBloc {
+  ClientsBloc() {
+    getClients();
+  }
+
   final _clientController = StreamController<List<Client>>.broadcast();
 
   get clients => _clientController.stream;
-
-  dispose() {
-    _clientController.close();
-  }
 
   getClients() async {
     _clientController.sink.add(await DBProvider.db.getAllClients());
   }
 
-  ClientsBloc() {
-    getClients();
+  Future<void> blockUnblock(Client client) async {
+    await DBProvider.db.blockOrUnblock(client);
+    await getClients();
   }
 
-  blockUnblock(Client client) {
-    DBProvider.db.blockOrUnblock(client);
-    getClients();
+  Future<void> delete(int id) async {
+    await DBProvider.db.deleteClient(id);
+    await getClients();
   }
 
-  delete(int id) {
-    DBProvider.db.deleteClient(id);
-    getClients();
+  Future<void> add(Client client) async {
+    await DBProvider.db.newClient(client);
+    await getClients();
   }
 
-  add(Client client) {
-    DBProvider.db.newClient(client);
-    getClients();
+  dispose() {
+    _clientController.close();
   }
 }
